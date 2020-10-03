f5ef4e8d28b708ad773339b37e1c140ac89dae26
Andreas Thomas <dev@chronark.com>
2020-10-03 11:34:45 +0200

    Improve a11y

--- components/Hamburger.tsx
+++ components/Hamburger.tsx
@@ -7,8 +7,9 @@ type Props = {
 const Hamburger = (props: Props) => {
     return (
         <button
+            aria-label="Menu"
             onClick={props.onClick}
-            className="z-50 inline-flex items-center justify-center block p-2 rounded-sm text-carbon-200 transition duration-150 ease-in-out hover:text-gray-500 hover:bg-gray-100 hover:outline-none focus:outline-none"
+            className="z-50 items-center justify-center block p-2 rounded-sm transition duration-150 ease-in-out text-carbon-200 hover:text-gray-500 hover:bg-gray-100 hover:outline-none focus:outline-none"
         >
             <svg className="w-6 h-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
