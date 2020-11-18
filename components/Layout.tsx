022635893f12a51da226374433aa81162d82c065
Andreas Thomas <dev@chronark.com>
2020-11-18 12:36:58 +0100

    Set min height to screen

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,6 @@
 const Layout = (props: any) => (
-    <div className="bg-carbon-900">
-        <div className="mx-auto max-w-screen-xl">{props.children}</div>
+    <div className="min-h-screen bg-carbon-900">
+        <div className="max-w-screen-xl mx-auto">{props.children}</div>
     </div>
 )
 
